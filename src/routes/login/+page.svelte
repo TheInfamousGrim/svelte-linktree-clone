4c12c9bd9d7dfe144b4f7d960e40da4dc4bb5913
George Fincher <george@grimfunky.dev>
2023-07-19 04:00:04 +0100

    Auth 🛂: Add google sign in button

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,4 +1,13 @@
 <script lang="ts">
+	import { auth } from '$lib/firebase';
+
+	import { GoogleAuthProvider, signInWithPopup, signOut } from 'firebase/auth';
+
+	async function signInWithGoogle() {
+		const provider = new GoogleAuthProvider();
+		const user = await signInWithPopup(auth, provider);
+		console.log(user);
+	}
 </script>
 
-<h2>Login - Sign In</h2>
+<button class="btn" on:click={signInWithGoogle}>Sign In With Google</button>
