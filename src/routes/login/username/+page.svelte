ec66c6c89ef45f006756daa1e589f5930b55c29b
George Fincher <george@grimfunky.dev>
2023-08-02 19:51:08 +0100

    Feat ✨: Implement username validation

--- src/routes/login/username/+page.svelte
+++ src/routes/login/username/+page.svelte
@@ -14,6 +14,13 @@
 	// Debounce timer
 	let debounceTimer: NodeJS.Timeout;
 
+	// Validate the username
+	const regex = /^(?=[a-zA-Z0-9._]{3,30}$)(?!.*[_.]{2})[^_.].*[^_.]$/;
+
+	$: isValidUsername = username?.length > 2 && username?.length < 30 && regex.test(username);
+	$: isTouched = username?.length > 0;
+	$: isTaken = isValidUsername && !isAvailable && !loading;
+
 	// Check if username is available
 	async function checkUsernameAvailability() {
 		isAvailable = false;
@@ -60,21 +67,36 @@
 	<form
 		action=""
 		on:submit|preventDefault={confirmUsername}
-		class="flex flex-col justify-center gap-2"
+		class="flex flex-col items-center justify-center gap-2"
 	>
 		<input
 			type="text"
 			placeholder="Username"
-			class="input input-bordered input-accent"
+			class="input input-bordered input-accent w-64 grow-0"
+			class:input-error={!isValidUsername && isTouched}
+			class:input-warning={isTaken}
+			class:input-success={isAvailable && isValidUsername && !loading}
 			bind:value={username}
 			on:input={checkUsernameAvailability}
 		/>
 
-		{#if isAvailable}
-			<p class="text-left text-success">Username is available!</p>
+		{#if loading}
+			<p class="text-secondary">Checking availability of @{username}</p>
+		{/if}
+
+		{#if !isValidUsername && isTouched}
+			<p class="text-error">
+				Username must be between 3 and 30 characters and can only contain letters, numbers,
+				underscores and periods
+			</p>
+		{/if}
+
+		{#if isTaken}
+			<p class="text-warning">Username @{username} is already taken</p>
+		{/if}
+
+		{#if isAvailable && isValidUsername && !loading}
 			<button class="btn btn-success">Confirm username @{username}</button>
-		{:else if username.length > 0}
-			<p class="mt-4 text-left text-error">Username is not available.</p>
 		{/if}
 	</form>
 </AuthCheck>
